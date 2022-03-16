71bac37f92073b8e8f05d2a4e79298228d8b08ff
EdgarSargsyan2001 <edgar.sargsyan.2001@mail.ru>
2022-03-16 18:30:23 +0400

    error delete

--- src/components/useFetchData.jsx
+++ src/components/useFetchData.jsx
@@ -1,16 +1,17 @@
 
-import { useEffect, useState } from 'react';
+import { useEffect, useMemo, useState } from 'react';
 
  // homework 12.2
 function useFetchData(){
   const [respVal,setRespVal] = useState()
-  const Names = ["akita","beagle","malamute","husky"]
+  
+
   useEffect(()=>{
-      fetch(`https://dog.ceo/api/breed/${Names.splice(parseInt( Math.random()*Names.length),1)}/images/random`).then(val =>{
+      fetch(`https://dog.ceo/api/breed/akita/images/random`).then(val =>{
         return val.json()
       }).then(response => setRespVal(response.message) )   
       
-    },[Names])
+    },[])
   
   return{respVal}
         
